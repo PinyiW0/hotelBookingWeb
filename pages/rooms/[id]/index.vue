0f77229c8026c27f1d789cac2164c218b28e11c4
Pinyi Wu <wpypy8@gmail.com>
2025-01-16 08:27:07 +0800

    feat: 新增客房旅宿＋詳細房型頁

--- pages/rooms/[id]/index.vue
+++ pages/rooms/[id]/index.vue
@@ -8,49 +8,193 @@ useSeoMeta({
   title: '房型詳細頁',
   description: '房型詳細頁',
 });
+/** 調整 header 背景色 */
+const headerBgColor = useState<string>('headerBgColor');
+headerBgColor.value = 'bg-gray-120';
+
+/** 房間資訊 */
+const roomInfoList: any[] = [
+  { iconName:'i-fluent:slide-size-24-filled', txt:'24 坪' },
+  { iconName:'i-material-symbols:bed-rounded', txt:'1 張大床' },
+  { iconName:'i-material-symbols:person', txt:'2-4 人' },
+];
+/** 訂房須知 */
+const knowList: any[] = [
+  { txt:'入住時間為下午3點，退房時間為上午12點。'},
+  { txt:'如需延遲退房，請提前與櫃檯人員聯繫，視當日房況可能會產生額外費用。'},
+  { txt:'請勿在房間內抽煙，若有抽煙需求，可以使用設在酒店各樓層的專用吸煙區。'},
+  { txt:'若發現房間內的設施有損壞或遺失，將會按照價值收取賠償金。'},
+  { txt:'請愛惜我們的房間與公共空間，並保持整潔。'},
+  { txt:'如需額外的毛巾、盥洗用品或其他物品，請聯繫櫃檯。'},
+  { txt:'我們提供免費的Wi-Fi，密碼可以在櫃檯或是房間內的資訊卡上找到。'},
+  { txt:'請勿帶走酒店房內的物品，如有需要購買，請與我們的櫃檯人員聯繫。'},
+  { txt:'我們提供24小時櫃檯服務，若有任何需求或疑問，歡迎隨時詢問。'},
+  { txt:'為了確保所有客人的安全，請勿在走廊或公共區域大聲喧嘩，並遵守酒店的其他規定。'},
+];
+/**房內設備 */
+const facilityInfo: any[] = [
+  { title:'平面電視', isProvide: true },
+  { title:'吹風機', isProvide: true },
+  { title:'冰箱', isProvide: true },
+  { title:'熱水壺', isProvide: true },
+  { title:'檯燈', isProvide: true },
+  { title:'衣櫃', isProvide: true },
+  { title:'除濕機', isProvide: true },
+  { title:'浴缸', isProvide: true },
+  { title:'書桌', isProvide: true },
+  { title:'音響', isProvide: true },
+];
+// 輪播圖
+const bannerList: any[] = [
+  { label: 'room1', imgPath: 'Room-0.png' },
+  { label: 'room2', imgPath: 'Room-1.png' },
+  { label: 'room3', imgPath: 'Room-2.png' },
+  { label: 'room4', imgPath: 'Room-3.png' },
+];
+const galleryData: any[] = [
+  [ // 第一列圖片
+    { src: '/images/Image/main-1.png', alt: '' },
+    { src: '/images/Image/Room0.png', alt: '' }
+  ],
+  [ // 第二列圖片
+    { src: '/images/Image/Room-3.png', alt: '' },
+    { src: '/images/Image/Room-1.png', alt: '' }
+  ]
+];
+
+const people = ref<number>(1);
+const handlePeople = (num: number) => {
+  const newVal = people.value + num;
+  if (newVal >= 1 && newVal <= 4) {
+    people.value = newVal;
+  };
+};
 </script>
 
 <template>
   <section class="w-full bg-primary-10">
     <!-- 照片區 -->
-    <div class="max-w-1760px mx-auto py-20 grid grid-cols-2 gap-2">
-      <!-- 左側主圖 -->
-      <div class="w-full max-h-150 rounded-l-5 overflow-hidden">
-        <img src="/images/Image/main-0.png" alt="" width="978px" class="object-cover" >
-      </div>
-      <!-- 右側組圖 -->
-      <div class="grid grid-cols-2 gap-2">
-        <!-- 第一列 -->
-        <div class="grid grid-cols-1 gap-2">
-          <div class="w-full h-74 overflow-hidden">
-            <img src="/images/Image/main-1.png" alt="" class="w-full h-full object-cover">
+     <div class="hidden lg:block">
+        <div class="px-4 max-w-1760px mx-auto pt-0 pb-10 lg:py-20 grid grid-cols-2 gap-2">
+          <!-- 左側主圖 -->
+          <div class="w-full max-h-150 rounded-l-5 overflow-hidden">
+            <img src="/images/Image/main-0.png" alt="" width="978px" class="object-cover" >
           </div>
-          <div class="w-full h-74 overflow-hidden">
-            <img src="/images/Image/Room0.png" alt="" class="w-full h-full object-cover">
-          </div>
-        </div>
-        <!-- 第二列 -->
-        <div class="grid grid-cols-1 gap-2">
-          <div class="w-full h-74 rounded-rt-5 overflow-hidden">
-            <img src="/images/Image/Room-3.png" alt="" class="w-full h-full object-cover">
-          </div>
-          <div class="w-full h-74 rounded-rb-5 overflow-hidden">
-            <img src="/images/Image/Room-1.png" alt="" class="w-full h-full object-cover">
+          <!-- 右側組圖 -->
+          <div class="grid grid-cols-2 gap-2">
+            <div v-for="(column, colIdx) in galleryData" :key="colIdx" class="grid gap-2">
+              <div v-for="(item, itemIdx) in column" :key="itemIdx" :class="{'rounded-tr-5':colIdx === 1 && itemIdx === 0, 'rounded-br-5':colIdx === 1 && itemIdx === 1}" class="w-full h-74 overflow-hidden">
+                <img :src="item.src" :alt="item.alt" class="w-full h-full object-cover">
+              </div>
+            </div>
           </div>
         </div>
+     </div>
+     <ClientOnly>
+      <div class="lg:hidden relative w-full h-full overflow-x-hidden overflow-y-visible">
+         <!-- swiper -->
+        <el-carousel :indicator-position="undefined" class="w-full !h-60 !sm:h-85 !md:h-115">
+          <el-carousel-item v-for="(slide, idx) in bannerList" :key="idx">
+            <!-- 輪播圖 -->
+            <img :src="`/images/Image/${slide.imgPath}`" :alt="slide.label" class="w-full h-full object-cover">
+          </el-carousel-item>
+        </el-carousel>
+        <DefaultBtn text="看更多" btnStyle="secondary" class="absolute right-0 bottom-0 z-5 m-5 font-bold" />
       </div>
-    </div>
+    </ClientOnly>
+
     <!-- 詳細資訊 -->
-    <div class="max-w-1296px mx-auto py-30">
-      <div class="flex gap-18">
+    <div class="px-4 pb-8 pt-10 xl:(px-4 py-30) max-w-1296px mx-auto">
+      <div class="w-full flex gap-0 lg:gap-18">
         <!-- 房間資訊 -->
-         <div class="flex flex-col"></div>
+         <div class="w-full xl:max-w-70% flex flex-col gap-6 xl:gap-20">
+          <div class="flex flex-col gap-4">
+            <h2 class="text-8 lg:text-12 font-bold">尊爵雙人房</h2>
+            <p class="text-3.5 lg:text-4 text-gray-80 leading-6">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
+          </div>
+          <!-- 房型基本資訊 -->
+          <div class="flex flex-col gap-4 xl:gap-6">
+            <severTitle title="房型基本資訊" />
+            <div class="flex gap-4">
+              <RoomInfoCard v-for="(item, idx) in roomInfoList" :key="idx" :icon-name="item.iconName" :txt="item.txt" />
+            </div>
+          </div>
+          <!-- 房間其他資訊 -->
+          <div class="flex flex-col gap-4 xl:gap-6">
+            <severTitle title="房內設備" />
+            <div class="p-6 w-full grid grid-cols-2 sm:grid-cols-5 gap-x-10 gap-y-2 bg-white rounded-5">
+              <RoomTagCard  v-for="i in facilityInfo" :title="i.title"  />
+            </div>
+          </div>
+          <!-- 訂房須知 -->
+           <div class="flex flex-col gap-6">
+            <severTitle title="訂房須知" />
+            <ul class="list-decimal pl-6 space-y-2">
+              <li v-for="(i, idx) in knowList" :key="idx" class="text-3.5 lg:text-4 text-gray-80 text-justify leading-5">{{ i.txt }}</li>
+            </ul>
+           </div>
+         </div>
         <!-- 預約房型卡 -->
-         <div class="flex flex-col bg-white rounded-5"></div>
+         <div class="hidden lg:block">
+           <div class="max-h-620px p-10 flex flex-col gap-3 bg-white rounded-5">
+            <h3 class="text-6 font-bold pb-4 border-b-(px solid gray-40)">預訂房型</h3>
+            <div class="mt-7 flex flex-col gap-2">
+              <h2 class="text-10 text-gray-80 font-bold tracking-wider leading-10">尊爵雙人房</h2>
+              <p class="text-4 text-gray-80 leading-6">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
+            </div>
+            <div class="mt-7 flex flex-col gap-4">
+              <!-- 日期 -->
+              <div class="flex items-center gap-2"></div>
+              <!-- 人數 -->
+              <div class="flex items-center justify-between">
+                <p class="py-4 text-4 font-bold">人數</p>
+                <div class="flex items-center">
+                  <button @click="handlePeople(-1)" :disabled="people <= 1" class="p-0 rounded-full border-(px solid gray-40) bg-white group">
+                    <div class="p-4 rounded-full cursor-pointer duration-300 group-hover:bg-gray-60">
+                      <div class="i-mdi:minus w-6 h-6 duration-300 group-hover:(text-white)" />
+                    </div>
+                  </button>
+                  <div class="w-16">
+                    <p class="text-5 text-center font-bold">{{ people }}</p>
+                  </div>
+                  <button @click="handlePeople(1)" :disabled="people >= 4" class="p-0 rounded-full border-(px solid gray-40) bg-white group">
+                    <div class="p-4 rounded-full cursor-pointer duration-300 group-hover:bg-primary">
+                      <div class="i-mdi:plus w-6 h-6 duration-300 group-hover:(text-white)" />
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div v-if="people <= 1" class="text-4 text-error text-right duration-300">人數不得小於1</div>
+            <div v-if="people >= 4" class="text-4 text-error text-right duration-300">人數不得大於4</div>
+            <p class="mt-7 text-4 xl:text-6 text-primary font-bold tracking-wider">NT$ 10,000</p>
+            <DefaultBtn text="立即預訂" class="mt-7 font-bold" />
+          </div>
+         </div>
       </div>
-      
     </div>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(.el-carousel__button) {
+  background-color: #F1EAE4;
+  border-radius: 100px;
+  height: 4px;
+  opacity: 1;
+}
+:deep(.el-carousel__indicator.is-active button ) {
+  background-color: #BF9D7D;
+  color:#BF9D7D;
+  width: 60px;
+}
+
+:deep(.el-carousel__container) {
+  height: 100% !important;
+}
+
+:deep(.el-carousel__indicators) {
+  bottom: 8px !important;
+  left: 110px;
+}
+</style>
--- pages/rooms/index.vue
+++ pages/rooms/index.vue
@@ -7,62 +7,106 @@ useSeoMeta({
   title: '客房旅宿頁',
   description: '客房旅宿頁',
 });
+
+// 輪播圖
+const bannerList: any[] = [
+  { label: 'room1', imgPath: 'Room-0.png' },
+  { label: 'room2', imgPath: 'Room-1.png' },
+  { label: 'room3', imgPath: 'Room-2.png' },
+  { label: 'room4', imgPath: 'Room-3.png' },
+];
+
+/** 房間資訊 */
+const roomInfoList: any[] = [
+  { iconName:'i-fluent:slide-size-24-filled', txt:'24 坪' },
+  { iconName:'i-material-symbols:bed-rounded', txt:'1 張大床' },
+  { iconName:'i-material-symbols:person', txt:'2-4 人' },
+];
+
+// 響應式高度，默認設置為空
+const carouselHeight = ref<string>('');
+const isDesktop = ref(false);
+
+// 更新高度和設備類型
+const updateResponsiveValues = () => {
+  if (typeof window !== 'undefined') {
+    const width = window.innerWidth;
+    if (width >= 1280) carouselHeight.value = '457px';
+    else if (width >= 768) carouselHeight.value = '280px';
+    else carouselHeight.value = '200px';
+
+    isDesktop.value = width >= 768;
+  }
+};
+
+// 客戶端執行
+onMounted(() => {
+  updateResponsiveValues(); // 初始化
+  window.addEventListener('resize', updateResponsiveValues);
+});
+
+// 清理事件監聽器
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateResponsiveValues);
+});
+
 </script>
 
 <template>
-  <section>
+  <section class="flex flex-col justify-center">
     <!-- swiper area -->
     <ClientOnly>
-      <div class="w-full h-full overflow-x-hidden overflow-y-visible">
-        <Carousel class="absolute top-0 left-0 z-1 3xl:-z-1" />
+      <div class="absolute top-0 left-0 w-full h-full overflow-x-hidden overflow-y-visible">
+         <!-- swiper -->
+        <el-carousel arrow="never" :indicator-position="undefined" class="w-full !h-466px !sm:h-120 !md:h-150 !lg:h-160 !xl:h-200">
+          <el-carousel-item v-for="(slide, idx) in bannerList" :key="idx">
+            <!-- 黑色透明遮罩 -->
+            <div class="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+            <!-- 輪播圖 -->
+            <img :src="`/images/Image/${slide.imgPath}`" :alt="slide.label" class="w-full h-full object-cover">
+          </el-carousel-item>
+        </el-carousel>
         <div
-          class="py-54 max-w-780px mx-auto flex items-center gap-20">
+          class="absolute top-1/4 md:top-1/3 left-1/2 -translate-1/2 py-54 max-w-780px mx-auto flex flex-col xl:(top-1/3 flex-row gap-20) items-center gap-5">
           <CarouselTitle class="pt-4"/>
           <h2 class="text-8 xl:(text-12 leading-25) text-white font-bold tracking-1 leading-15 whitespace-nowrap">客房旅宿</h2>
         </div>
       </div>
     </ClientOnly>
     <!-- 房型挑選 -->
-     <div class="py-30 w-full bg-primary-10">
-       <div class="max-w-1296px flex flex-col items-center gap-20">
+     <div class="pt-120 md:pt-150 lg:pt-155 xl:pt-200 py-30 w-full bg-primary-10 flex justify-center">
+       <div class="max-w-1296px flex flex-col items-center xl:(items-center justify-center) gap-20">
         <!-- title -->
-        <div class="px-3 flex flex-col gap-4">
+        <div class="px-3 xl:px-0 w-full flex flex-col justify-center gap-4">
           <p class="text-3.5 xl:text-5 text-gray-80 font-bold">房型選擇</p>
           <h2 class="text-8 xl:text-12 text-primary font-bold">各種房型，任您挑選</h2>
         </div>
         <!-- card -->
-         <div class="px-3 w-full flex flex-col xl:(flex-row justify-center) items-center gap-12">
-          <div class="flex flex-col items-center rounded-5 overflow-hidden bg-white cursor-pointer duration-300 hover:opacity-70">
+         <div class="px-3 w-full flex flex-col xl:(px-0 flex-row) items-center gap-12">
+          <div class="w-full h-auto flex flex-col xl:(flex-row justify-center) items-center rounded-5 overflow-hidden bg-white">
             <!-- swiper -->
-             <div class="w-full h-200px bg-primary">swiper佔位</div>
-             <div class="p-4 flex flex-col gap-6">
+            <el-carousel :arrow="isDesktop ? 'always' : 'never'" class="w-full !h-50 !sm:h-70 !xl:(w-773px h-457px)">
+              <el-carousel-item v-for="item in 4" :key="item">
+                <img src="/images/Image/Room.png" alt="" class="w-full h-full xl:min-h-457px object-cover block">
+              </el-carousel-item>
+            </el-carousel>
+             <div class="p-4 xl:p-10 flex flex-col gap-6">
               <!-- header -->
               <div class="flex flex-col gap-2">
                 <h3 class="text-7 font-bold">尊爵雙人房</h3>
-                <p class="text-3.5 xl:text-5 text-gray-80 leading-5">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
+                <p class="text-3.5 xl:(text-5 leading-7) text-gray-80 leading-5">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
               </div>
               <!-- body -->
-               <div class="w-full flex items-center justify-center gap-4">
-                <!-- TODO:切成元件 -->
-                <div class="px-4 py-5 w-97px h-97px flex flex-col gap-2 bg-white border-(px solid primary-40) rounded-2">
-                  <i class="i-fluent:slide-size-24-filled w-6 h-6 text-primary" />
-                  <p class="text-3.5 xl:text-5 text-gray-80 font-bold">24 坪</p>
-                </div>
-                <div class="px-4 py-5 w-97px h-97px flex flex-col gap-2 bg-white border-(px solid primary-40) rounded-2">
-                  <i class="i-fluent:slide-size-24-filled w-6 h-6 text-primary " />
-                  <p class="text-3.5 xl:text-5 text-gray-80 font-bold">24 坪</p>
-                </div>
-                <div class="px-4 py-5 w-97px h-97px flex flex-col gap-2 bg-white border-(px solid primary-40) rounded-2">
-                  <i class="i-fluent:slide-size-24-filled w-6 h-6 text-primary " />
-                  <p class="text-3.5 xl:text-5 text-gray-80 font-bold">24 坪</p>
+               <div class="w-full flex items-center justify-start gap-4">
+                <div class="flex gap-4">
+                  <RoomInfoCard v-for="(item, idx) in roomInfoList" :key="idx" :icon-name="item.iconName" :txt="item.txt" />
                 </div>
                </div>
-               <div class="w-319px 2xl:w-500px 3xl:w-636px h-2px rounded-full bg-gradient-to-r from-primary to-[#FFFFFF]"></div>
-              <!-- footer TODO:改成 nuxtlink -->
-               <div class="py-4 flex justify-between">
-                <p class="text-4 xl:text-6 text-primary font-bold tracking-widest">NT$ 10,000</p>
-                <i class="i-fluent:arrow-right-24-filled w-6 h-6 text-primary cursor-pointer duration-300 hover:text-primary-120" />
-               </div>
+               <div class="linePrimary w-319px 2xl:w-500px 3xl:w-636px h-2px rounded-full bg-gradient-to-r from-primary to-[#FFFFFF]"></div>
+               <NuxtLink role="link" class="py-4 flex justify-between cursor-pointer group">
+                <p class="text-4 xl:text-6 text-primary font-bold tracking-widest duration-300 group-hover:text-primary-120">NT$ 10,000</p>
+                <i class="i-fluent:arrow-right-24-filled w-6 h-6 text-primary duration-300 group-hover:text-primary-120" />
+               </NuxtLink>
              </div>
           </div>
          </div>
@@ -71,4 +115,49 @@ useSeoMeta({
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* 線條 自定義動畫 */
+@layer utilities {
+  @keyframes linePrimary {
+    0%,
+    100% {
+      transform: scaleX(0.3);
+      opacity: 10;
+    }
+
+    50% {
+      transform: scaleX(1);
+      opacity: 1;
+    }
+  }
+
+  .linePrimary {
+    animation: linePrimary 2.5s infinite ease-in-out;
+    transform-origin: left;
+  }
+}
+
+:deep(.el-carousel__arrow) {
+  color: #4B4B4B;
+  background-color: #FFF;
+}
+:deep(.el-carousel__button) {
+  background-color: #FFF;
+  border-radius: 100px;
+  height: 4px;
+  opacity: 1;
+}
+:deep(.el-carousel__indicator.is-active button ) {
+  background-color: #BF9D7D;
+  color:#BF9D7D;
+}
+
+:deep(.el-carousel__container) {
+  height: 100% !important;
+}
+
+:deep(.el-carousel__indicators) {
+  bottom: 24px !important;
+  justify-content: center;
+}
+</style>
