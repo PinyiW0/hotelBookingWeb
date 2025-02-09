9ddaf13106410a7a83e6f3504c3a6d6bb54e4cfe
pinyi <wpypy8@gmail.com>
2025-02-09 21:45:28 +0800

    feat: 新增 Tab 元件

---
+++ components/Tab.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+
+interface OptionField {
+  title: string;
+  val: string;
+  disabled?: boolean;
+};
+
+const modelValue = defineModel();
+defineProps({
+  list: { type: Array as PropType<OptionField[]>, default: [] },
+  tabWidth: {
+    type: String,
+    default: 'px-6'
+  }
+});
+const emits = defineEmits(['update:modelValue']);
+const handleChangeTab = (item: any) => {
+  emits('update:modelValue', item.val)
+};
+</script>
+
+<template>
+  <div class="w-full flex items-center">
+    <button @click="handleChangeTab(item)" v-for="item in list" :key="item.val"
+      :class="[modelValue === item.val ? 'text-primary cursor-default' : 'text-white cursor-pointer', tabWidth]"
+      class="py-4 bg-transparent border-0">
+      <p :class="[modelValue === item.val ? `after:(absolute content-empty -bottom-3.5 left-1/2 -translate-x-1/2 w-8 h-1 bg-primary rounded-full)` : '']"
+        class="relative text-3.5 xl:text-4 text-center font-bold whitespace-nowrap tracking-wide">
+        {{ item.title }}
+      </p>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
