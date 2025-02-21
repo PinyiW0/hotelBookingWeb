41c35c926ac45c46f245cb0f39d4334130921d21
pinyi <wpypy8@gmail.com>
2025-02-21 17:46:34 +0800

    feat: 新增登入功能

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+const api = useApi();
+const { $swal } = useNuxtApp() as any;
+import { useRouter } from 'vue-router';
+const router = useRouter();
 import type { FormRules } from 'element-plus';
+import { ElForm } from 'element-plus';
+
 defineOptions({
   name: 'Login'
 });
@@ -10,31 +16,81 @@ useSeoMeta({
 definePageMeta({
   layout: 'login',
 });
+
 /** form */
-interface LoginInForm{
+interface LoginInForm {
   email: string;
   password: string;
 };
-const form = ref<LoginInForm>({
-  email:'',
+const loginForm = ref<InstanceType<typeof ElForm> | null>(null);
+const formTemplate = {
+  email: '',
   password: '',
-});
+}
+const temp = ref<any>(JSON.parse(JSON.stringify(formTemplate)));
+const rememberAccount = ref(false);
+
 /** 驗證 */
 const rules = reactive<FormRules>({
-  email:[
-    {required: true, message:' 電子信箱為必填', trigger:['blur', 'change']},
-    {type:'email', message:' 電子信箱格式錯誤', trigger:['blur', 'change']},
+  email: [
+    { required: true, message: ' 電子信箱為必填', trigger: ['blur', 'change'] },
+    { type: 'email', message: ' 電子信箱格式錯誤', trigger: ['blur', 'change'] },
   ],
-  password:[{required: true, message:' 密碼為必填', trigger:['blur', 'change']}]
+  password: [{ required: true, message: ' 密碼為必填', trigger: ['blur', 'change'] }]
 });
 
-const rememberAccount = ref(false);
+/** 登入 */
+const handleLogin = () => {
+  if (!loginForm.value) return;
+  loginForm.value.validate(async (isValid: boolean) => {
+    if (isValid) {
+      // 處理記住帳號邏輯
+      if (rememberAccount.value) {
+        localStorage.setItem('loginInfo', JSON.stringify(temp.value));
+      } else {
+        localStorage.removeItem('loginInfo');
+      };
+
+      await api.Users.Login(temp.value);
+      await $swal.fire({
+        icon: 'success',
+        iconColor: '#52DD7E',
+        title: '登入成功！',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true
+      });
+      // 清空表單
+      if (!rememberAccount.value && loginForm.value) {
+        loginForm.value.resetFields();
+      }
+      router.push('/');
+    } else {
+      $swal.fire({
+        icon: 'error',
+        iconColor: '#DA3E51',
+        title: '登入失敗！',
+        text: '請稍後再試，或是聯絡客服',
+        showConfirmButton: true
+      });
+    }
+  })
+};
+/** 載入記住的帳號資訊 */
+onMounted(() => {
+  const savedInfo = localStorage.getItem('loginInfo');
+  if (savedInfo) {
+    temp.value = JSON.parse(savedInfo);
+    rememberAccount.value = true;
+  }
+});
 </script>
 
 <template>
   <div class="relative grid grid-cols-1 xl:grid-cols-2 h-screen">
     <!-- deco line -->
-    <img src="/images/deco/login-line-web.svg" aria-hidden="true" class="hidden xl:block absolute top-10 sm:top-0 right-0 w-1/2">
+    <img src="/images/deco/login-line-web.svg" aria-hidden="true"
+      class="hidden xl:block absolute top-10 sm:top-0 right-0 w-1/2">
     <img src="/images/deco/login-line.svg" aria-hidden="true" class="xl:hidden absolute top-10 sm:top-0 w-full">
     <!-- bg -->
     <div class="max-w-960px overflow-hidden">
@@ -48,19 +104,20 @@ const rememberAccount = ref(false);
         <h2 class="text-8 text-white font-bold tracking-wide">立即開始旅程</h2>
       </div>
       <!-- form -->
-      <el-form :model="form":rules="rules" class="flex flex-col gap-4">
+      <el-form ref="loginForm" :model="temp" :rules="rules" class="flex flex-col gap-4">
         <el-form-item label="電子信箱" label-position="top" prop="email">
-          <el-input v-model="form.email"  placeholder="請輸入電子信箱" />
+          <el-input v-model="temp.email" placeholder="請輸入電子信箱" />
         </el-form-item>
-        <el-form-item label="密碼" label-position="top"  prop="password">
-          <el-input v-model="form.password" type="password" placeholder="請輸入密碼" show-password />
+        <el-form-item label="密碼" label-position="top" prop="password">
+          <el-input v-model="temp.password" type="password" placeholder="請輸入密碼" show-password />
         </el-form-item>
       </el-form>
       <!-- submit -->
-      <DefaultBtn to="/register" text="會員登入" class="font-bold" />
+      <DefaultBtn @click="handleLogin" text="會員登入" class="font-bold" />
       <div class="flex items-center justify-between">
         <el-checkbox v-model="rememberAccount" label="記住帳號" size="large" class="custom-checkbox" />
-        <NuxtLink to="/forget" class="text-primary text-3.5 fw-bold cursor-pointer duration-300 hover:opacity-60">忘記密碼?</NuxtLink>
+        <NuxtLink to="/forget" class="text-primary text-3.5 fw-bold cursor-pointer duration-300 hover:opacity-60">忘記密碼?
+        </NuxtLink>
       </div>
       <!-- go to signup -->
       <div class="flex items-center gap-2">
@@ -72,14 +129,16 @@ const rememberAccount = ref(false);
 </template>
 
 <style scoped>
-:deep(.el-form-item__label){
+:deep(.el-form-item__label) {
   color: white;
 }
-:deep(.el-checkbox){
+
+:deep(.el-checkbox) {
   color: white;
   font-size: 16px;
   font-weight: bold;
 }
+
 /* 勾選狀態的框樣式 */
 :deep(.custom-checkbox .el-checkbox__inner) {
   padding: 10px;
@@ -88,15 +147,18 @@ const rememberAccount = ref(false);
   background-color: white;
   transition: all 0.3s ease;
 }
+
 :deep(.custom-checkbox .el-checkbox__input.is-checked .el-checkbox__inner) {
   background-color: #BF9D7D;
   border-color: #BF9D7D;
 }
+
 /* 勾選狀態label */
 :deep(.custom-checkbox .el-checkbox__label) {
   color: white !important;
   font-size: 14px !important;
 }
+
 /* 勾勾樣式 */
 :deep(.custom-checkbox .el-checkbox__input.is-checked .el-checkbox__inner::after) {
   padding-top: 4px;
