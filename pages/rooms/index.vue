261519f7d34bcdb93b6f556ca7f39cfcae78e44c
pinyi <wpypy8@gmail.com>
2025-02-26 10:45:29 +0800

    feat: 新增客房旅宿頁 API 功能

--- pages/rooms/index.vue
+++ pages/rooms/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const api = useApi();
+
 defineOptions({
   name: 'RoomIndex'
 });
@@ -8,21 +9,22 @@ useSeoMeta({
   description: '客房旅宿頁',
 });
 
-// 輪播圖
-const bannerList: any[] = [
-  { label: 'room1' },
-  { label: 'room2' },
-  { label: 'room3' },
-  { label: 'room4' },
-];
-
 /** 房間資訊 */
-const roomInfoList: any[] = [
-  { iconName: 'i-fluent:slide-size-24-filled', txt: '24 坪' },
-  { iconName: 'i-material-symbols:bed-rounded', txt: '1 張大床' },
-  { iconName: 'i-material-symbols:person', txt: '2-4 人' },
-];
+const roomList = ref<any[]>([]);
+const getList = async () => {
+  const { result = null } = await api.Rooms.Load();
+  roomList.value = result.map((room: any) => ({
+    ...room,
+    roomInfoList: [
+      { iconName: 'i-fluent:slide-size-24-filled', txt: room.areaInfo },
+      { iconName: 'i-material-symbols:bed-rounded', txt: room.areaInfo },
+      { iconName: 'i-material-symbols:person', txt: `${room.maxPeople}人` },
+    ]
+  }));
+};
+getList();
 
+// #region Swiper
 // 響應式高度，默認設置為空
 const carouselHeight = ref<string>('');
 const isDesktop = ref(false);
@@ -39,7 +41,6 @@ const updateResponsiveValues = () => {
   }
 };
 
-// 客戶端執行
 onMounted(() => {
   updateResponsiveValues(); // 初始化
   window.addEventListener('resize', updateResponsiveValues);
@@ -49,7 +50,7 @@ onMounted(() => {
 onBeforeUnmount(() => {
   window.removeEventListener('resize', updateResponsiveValues);
 });
-
+// #endregion Swiper
 </script>
 
 <template>
@@ -60,11 +61,11 @@ onBeforeUnmount(() => {
         <!-- swiper -->
         <el-carousel arrow="never" :indicator-position="undefined"
           class="w-full !h-466px !sm:h-120 !md:h-150 !lg:h-160 !xl:h-200">
-          <el-carousel-item v-for="(slide, idx) in bannerList" :key="idx">
+          <el-carousel-item v-for="idx in 4" :key="idx">
             <!-- 黑色透明遮罩 -->
-            <div class="absolute top-0 left-0 w-full h-full bg-black/60"></div>
+            <div class="absolute top-0 left-0 w-full h-full bg-black/60" />
             <!-- 輪播圖 -->
-            <img :src="`/images/Image/Room-${idx}.png`" :alt="slide.label" class="w-full h-full object-cover">
+            <img :src="`/images/Image/Room-${idx - 1}.png`" :alt="`room${idx}`" class="w-full h-full object-cover">
           </el-carousel-item>
         </el-carousel>
         <div
@@ -75,6 +76,7 @@ onBeforeUnmount(() => {
         </div>
       </div>
     </ClientOnly>
+
     <!-- 房型挑選 -->
     <div class="pt-120 md:pt-150 lg:pt-155 xl:pt-200 py-30 w-full bg-primary-10 flex justify-center">
       <div class="max-w-1296px flex flex-col items-center xl:(items-center justify-center) gap-20">
@@ -84,37 +86,40 @@ onBeforeUnmount(() => {
           <h2 class="text-8 xl:text-12 text-primary font-bold">各種房型，任您挑選</h2>
         </div>
         <!-- card -->
-        <div class="px-3 w-full flex flex-col xl:(px-0 flex-row) items-center gap-12">
+        <div v-for="item in roomList" :key="item._id"
+          class="px-3 w-full flex flex-col xl:(px-0 flex-row) items-center gap-12">
           <div
             class="w-full h-auto flex flex-col xl:(flex-row justify-center) items-center rounded-5 overflow-hidden bg-white">
             <!-- swiper -->
-            <el-carousel :arrow="isDesktop ? 'always' : 'never'" class="w-full !h-50 !sm:h-70 !xl:(w-773px h-457px)">
-              <el-carousel-item v-for="item in 4" :key="item">
-                <img src="/images/Image/Room-1.png" alt="" class="w-full h-full xl:min-h-457px object-cover block">
+            <el-carousel :arrow="isDesktop ? 'always' : 'never'"
+              class="w-full !h-50 !sm:h-70 !xl:(w-773px h-457px flex-1)">
+              <el-carousel-item v-for="(img, idx) in item.imageUrlList" :key="idx">
+                <img :src="img" :alt="item.name" class="w-full h-full xl:min-h-457px object-cover block">
               </el-carousel-item>
             </el-carousel>
-            <div class="p-4 xl:p-10 flex flex-col gap-6">
+            <div class="p-4 xl:(p-10 flex-1) flex flex-col gap-6">
               <!-- header -->
               <div class="flex flex-col gap-2">
-                <h3 class="text-7 font-bold">尊爵雙人房</h3>
-                <p class="text-3.5 xl:(text-5 leading-7) text-gray-80 leading-5">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
+                <h3 class="text-7 font-bold">{{ item.name }}</h3>
+                <p class="text-3.5 xl:(text-5 leading-7) text-gray-80 leading-5">{{ item.description }}</p>
               </div>
               <!-- body -->
               <div class="w-full flex items-center justify-start gap-4">
                 <div class="flex gap-4">
-                  <RoomInfoCard v-for="(item, idx) in roomInfoList" :key="idx" :icon-name="item.iconName"
-                    :txt="item.txt" />
+                  <RoomInfoCard v-for="(info, idx) in item.roomInfoList" :key="idx" :icon-name="info.iconName"
+                    :txt="info.txt" />
                 </div>
               </div>
               <div
                 class="linePrimary w-319px 2xl:w-500px 3xl:w-636px h-2px rounded-full bg-gradient-to-r from-primary to-[#FFFFFF]">
               </div>
-              <NuxtLink role="link" class="py-4 flex justify-between cursor-pointer group">
+              <NuxtLink :to="`/rooms/${item._id}`" role="link"
+                class="py-4 flex justify-between cursor-pointer decoration-none group">
                 <p
                   class="text-4 xl:text-6 text-primary font-bold tracking-widest duration-300 group-hover:text-primary-120">
-                  NT$ 10,000</p>
+                  NT$ {{ item.price }}</p>
                 <i
-                  class="i-fluent:arrow-right-24-filled w-6 h-6 text-primary duration-300 group-hover:text-primary-120" />
+                  class="i-fluent:arrow-right-24-filled w-6 h-6 text-primary duration-300 group-hover:(text-primary-120 scale-120 translate-x-2)" />
               </NuxtLink>
             </div>
           </div>
@@ -124,7 +129,7 @@ onBeforeUnmount(() => {
   </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 /* 線條 自定義動畫 */
 @layer utilities {
   @keyframes linePrimary {
@@ -150,6 +155,10 @@ onBeforeUnmount(() => {
 :deep(.el-carousel__arrow) {
   color: #4B4B4B;
   background-color: #FFF;
+
+  &:hover {
+    opacity: 0.7;
+  }
 }
 
 :deep(.el-carousel__button) {
