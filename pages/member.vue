278926a44a18937486c56954fd84f5434984651e
Pinyi Wu <wpypy8@gmail.com>
2025-03-05 17:55:28 +0800

    fix: 調整會員頁路由＋修正 tab 切換功能

--- pages/member.vue
+++ pages/member.vue
@@ -1,7 +1,18 @@
-<!-- 關於臺藝的 MenuList -->
 <script setup lang="ts">
+import { useUserInfoStore } from '@/stores/userInfo';
+import { useRoute, useRouter } from 'vue-router';
+
 const router = useRouter();
 const route = useRoute();
+const userStore = useUserInfoStore();
+
+onMounted(() => {
+  userStore.getUserInfo();
+});
+
+/** 會員資料 */
+const userName = computed(() => userStore.user?.name || '尊榮貴賓');
+const userId = computed(() => userStore.user?.id || route.params.id);
 
 /** Tab */
 const tabList = ref<any[]>([
@@ -9,15 +20,17 @@ const tabList = ref<any[]>([
   { title: '我的訂單', val: 'myOrder' },
 ]);
 
-// 根據當前路由設定預設 tab
-const defaultTab = computed(() => {
-  return route.path.includes('/member/order') ? 'myOrder' : 'personalData';
-});
+// 設定當前 tab
+const activeTab = ref(route.path.includes('/order') ? 'myOrder' : 'personalData');
 
-//更換 Tab
+// 更換 Tab
 const handleChangeTab = (val: string) => {
-  val === 'personalData' ? router.push('/member') : router.push('/member/order')
+  if (!userId.value) return;
+  router.push(val === 'personalData' ? `/member/${userId.value}` : `/member/${userId.value}/order`)
 };
+watch(() => route.path, (newPath) => {
+  activeTab.value = newPath.includes('/order') ? 'myOrder' : 'personalData';
+});
 </script>
 
 <template>
@@ -29,11 +42,11 @@ const handleChangeTab = (val: string) => {
         <div class="w-18 h-18 xl:(w-36 h-36)">
           <img src="/images/Image/memeberIcon.png" alt="會員照片" class="w-full h-full object-cover">
         </div>
-        <h2 class="text-8 xl:text-12 text-white font-bold tracking-wider">Hello，Jessica</h2>
+        <h2 class="text-8 xl:text-12 text-white font-bold tracking-wider">Hello，{{ userName }}</h2>
       </div>
     </div>
     <div class="px-3 py-10 w-full flex flex-col gap-10 xl:(pt-20 pb-30 max-w-1296px mx-auto gap-20)">
-      <Tab @update:model-value="handleChangeTab" v-model="defaultTab" :list="tabList" />
+      <Tab @update:model-value="handleChangeTab" v-model="activeTab" :list="tabList" />
       <NuxtPage :key="$route.fullPath" />
     </div>
     <!-- DECO -->
