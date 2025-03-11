4162b94a329833f1df0a7f0257ab279075208e19
pinyi <wpypy8@gmail.com>
2025-03-11 16:47:41 +0800

    feat: 新增 web 版 header 會員 dropdown 功能

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { useUserInfoStore } from '@/stores/userInfo';
+import { useRouter } from "vue-router";
+const router = useRouter();
+
 /** @const open menu */
 const openMenu = ref<boolean>(false);
 const userStore = useUserInfoStore();
@@ -17,11 +20,42 @@ const baseMenuItem: MenuItem[] = [
 ];
 
 const authMenuItem = computed<MenuItem>(() => {
-  return userStore.user ? { label: userStore.user.name, path: '/member', icon: 'i-mdi:person-circle-outline' } : { label: '會員登入', path: '/login' }
+  return userStore.user ? { label: userStore.user.name, path: `/member/${userStore.user.id}`, icon: 'i-mdi:person-circle-outline' } : { label: '會員登入', path: '/login' }
 });
 
 const menuList = computed(() => {
   return [baseMenuItem[0], authMenuItem.value, baseMenuItem[1]];
+});
+
+/** 登入後 dropdown 顯示 */
+const isDropdown = ref(false);
+const isMobDropdown = ref(false);
+let dropdownTimeout: ReturnType<typeof setTimeout> | null = null;
+// Web 滑入滑出
+const openDropdown = () => {
+  if (dropdownTimeout) clearTimeout(dropdownTimeout); // 清除關閉計時器
+  isDropdown.value = true;
+};
+const closeDropdown = () => {
+  dropdownTimeout = setTimeout(() => {
+    isDropdown.value = false;
+  }, 200);
+};
+// Mob 點擊展開收回
+const toggleMobDropdown = (event: Event) => {
+  const target = event.target as HTMLElement;
+  if (!target.closest(".mobile-dropdown")) {
+    isMobDropdown.value = false;
+  }
+};
+/** 登出 */
+const logout = () => {
+  userStore.clearUserInfo();
+  router.push('/')
+};
+
+onMounted(() => {
+  document.addEventListener("click", closeDropdown);
 });
 </script>
 
@@ -30,9 +64,9 @@ const menuList = computed(() => {
     class="relative fixed top-0 z-60 left-0 w-full h-18 lg:h-40 px-3 lg:px-20 py-4 flex items-center justify-between">
     <!-- logo -->
     <h1>
-      <a href="/">
+      <NuxtLink to="/">
         <img src="/images/LOGO.svg" alt="享樂酒店 Enjoyment Luxury Hotel" class="h-10 lg:h-18 cursor-pointer">
-      </a>
+      </NuxtLink>
     </h1>
 
     <!-- phone menu icon -->
@@ -69,15 +103,35 @@ const menuList = computed(() => {
       </ul>
     </transition>
 
+    <!-- mob Dropdown -->
+    <transition name="slide-down">
+      <div v-if="isMobDropdown">123</div>
+    </transition>
+
     <!-- web menu list -->
     <div class="hidden lg:block">
       <ul class="flex items-center gap-4">
-        <li v-for="(item, idx) in menuList" :key="idx" class="p-4 list-none">
+        <li v-for="(item, idx) in menuList" :key="idx" class="relative p-4 list-none">
           <!-- 會員登入文字＋頭貼 -->
           <template v-if="item.icon">
-            <NuxtLink :to="item.path" class="flex items-center gap-1 decoration-none">
-              <div :class="item.icon" class="text-white w-6 h-6" />
-              <p class="text-base text-white font-bold">{{ item.label }}</p>
+            <NuxtLink :to="item.path" @mouseenter="openDropdown" @mouseleave="closeDropdown"
+              class="flex items-center gap-1 decoration-none group">
+              <div :class="item.icon" class="text-white w-6 h-6 group-hover:opacity-70" />
+              <p class="text-base text-white font-bold group-hover:opacity-70">{{ item.label }}</p>
+              <!-- web Dropdown -->
+              <transition name="fade">
+                <div v-if="isDropdown"
+                  class="absolute top-full -left-35 z-10 mt-3 w-260px h-136px flex flex-col justify-center bg-white rounded-5 shadow-lg">
+                  <NuxtLink v-if="userStore.user" :to="`/member/${userStore.user.id}`"
+                    class="px-6 py-4 w-full text-4 text-gray-80 font-bold decoration-none cursor-pointer duration-300 hover:(text-primary bg-primary-10)">
+                    我的帳戶
+                  </NuxtLink>
+                  <button @click.stop="logout"
+                    class="px-6 py-4 w-full text-4 text-left text-gray-80 font-bold border-0 bg-white cursor-pointer duration-300 hover:(text-primary bg-primary-10)">
+                    登出
+                  </button>
+                </div>
+              </transition>
             </NuxtLink>
           </template>
           <!-- 預設按鈕樣式 -->
@@ -96,5 +150,3 @@ const menuList = computed(() => {
     </div>
   </header>
 </template>
-
-<style scoped></style>
