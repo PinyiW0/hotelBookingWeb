03afeaa9dbb25081fe0645530b579147fdbcdfdf
pinyi <wpypy8@gmail.com>
2025-03-13 16:11:09 +0800

    style: 優化細節

--- pages/index.vue
+++ pages/index.vue
@@ -50,8 +50,8 @@ loadData();
 /** 交通方式 */
 const trafficInfo: any[] = [
   { title: '自行開車', content: '如果您選擇自行開車，可以透過國道一號下高雄交流道，往市區方向行駛，並依路標指示即可抵達「享樂酒店」。飯店內設有停車場，讓您停車方便。', icon: 'i-mdi:car' },
-  { title: '高鐵/火車', content: '如果您是搭乘高鐵或火車，可於左營站下車，外頭有計程車站，搭乘計程車約20分鐘即可抵達。或者您也可以轉乘捷運紅線至中央公園站下車，步行約10分鐘便可抵達。', icon: 'i-material-symbols:train' },
-  { title: '禮賓車服務', content: '承億酒店提供禮賓專車接送服務，但因目的地遠近會有不同的收費，請撥打電話將由專人為您服務洽詢專線：(07)123-4567', icon: 'i-mdi:car-hatchback' },
+  { title: '高鐵/火車', content: '如果您是搭乘高鐵或火車，可於左營站下車，外頭有計程車站，搭乘計程車約 20 分鐘即可抵達。或者您也可以轉乘捷運紅線至中央公園站下車，步行約 10 分鐘便可抵達。', icon: 'i-material-symbols:train' },
+  { title: '禮賓車服務', content: '享樂酒店提供禮賓專車接送服務，但因目的地遠近會有不同的收費，請撥打電話將由專人為您服務洽詢專線： ( 07 ) 123-4567', icon: 'i-mdi:car-hatchback' },
 ];
 // #endregion 獲取資料
 const bannerList: any[] = [
