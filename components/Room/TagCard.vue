e763b6643847ea84209ecd3a1714c8a5d391d413
pinyi <wpypy8@gmail.com>
2025-03-18 15:34:29 +0800

    fix: 修正 icon 大小不一問題

--- components/Room/TagCard.vue
+++ components/Room/TagCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps({
-  title:{
+  title: {
     type: String,
     required: true,
   }
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="flex items-center gap-2">
-    <div class="i-mdi:check w-6 h-6 text-primary" />
+    <div class="i-mdi:check w-6 h-6 text-primary shrink-0" />
     <p class="text-3.5 xl:text-4 text-gray-80 font-bold whitespace-nowrap">{{ title }}</p>
   </div>
 </template>
